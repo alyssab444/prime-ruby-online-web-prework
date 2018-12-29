7fc4b3ccd7e0c02d1453572c2af3d772bd2aea35
Alyssa Brown <alyssa.brown@live.com>
2018-12-29 17:00:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime? (int)
-    if n <= 1
+    if int <= 1
         false
-    elsif n == 2
+    elsif int == 2
         true
     else 
-        (2..n/2).none? { |i| n % i == 0}
+        (2..int/2).none? { |i| int % i == 0}
     end
 end 
